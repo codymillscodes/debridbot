20891cb34628ce805aa5825a76cd6adab26543ce
idiotdoomspiral <vzualbandt@gmail.com>
2022-04-21 06:27:15 -0700

    mostly rewritten

--- Cogs/debrid.py
+++ Cogs/debrid.py
@@ -1,51 +1,105 @@
-import discord
-from discord.ext import commands
 import json
+
+import discord
 import requests
-import os
+from discord.ext import commands
 from py1337x import py1337x
 
-with open("configuration.json", "r") as config: 
-	data = json.load(config)
-	debrid_key = data["debrid_key"]
-	debrid_host = data["debrid_host"]
+with open("configuration.json", "r") as config:
+    data = json.load(config)
+    debrid_key = data["debrid_key"]
+    debrid_host = data["debrid_host"]
 
-torrents = py1337x(proxy='1337x.to', cache='cache', cacheTime=5000)
+torrents = py1337x(proxy="1337x.to", cache="cache", cacheTime=5000)
 
 class DebridCog(commands.Cog, name="debrid commands"):
-    def __init__(self, bot:commands.Bot):
-	    self.bot = bot
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
-@commands.command(name='search', help='search 1337x')
-async def search(ctx, arg):
-    torrentResults = torrents.search(arg, sortBy='seeders', order='desc')
-    await ctx.send(torrents.info(torrentId=torrentResults['items'][0]['torrentId'])['magnetLink'])
+    def addMagnet(ctx, magnet):
+        url = str(
+            "https://api.alldebrid.com/v4/magnet/upload?agent="
+            + debrid_host
+            + "&apikey="
+            + debrid_key
+            + "&magnets[]="
+            + magnet
+        )
+        print("magnet added")
+        return json.loads(requests.get(url).text)["data"]["magnets"]
+#       add and return magnet link
 
 
-@commands.command(name='magnet', help='add magnet link')
-async def addMagnet(self, ctx, arg):
-    url = str("https://api.alldebrid.com/v4/magnet/upload?agent=" + debrid_host + '&apikey=' + debrid_key + '&magnets[]=' + arg)
-    r = requests.get(url)
-    t = json.loads(r.text)["data"]["magnets"]
-    ident = str(t[0]["id"])
+    def unlockLink(ctx, link): 
+        url = str(
+            "https://api.alldebrid.com/v4/link/unlock?agent="
+            + debrid_host
+            + "&apikey="
+            + debrid_key
+            + "&link="
+            + link
+        )
+        print("unlocking")
+        return json.loads(requests.get(url).text)["data"]["link"]
 
-    if t[0]["ready"]:
-        url = str('https://api.alldebrid.com/v4/magnet/status?agent=' + debrid_host + '&apikey=' + debrid_key + '&id=' + ident)
-        r = requests.get(url)       
-        t = json.loads(r.text)["data"]["magnets"]["links"]
+    def getMagnetId(ctx, magnet):
+        return str(magnet[0]["id"])
+
+    def getUnhostedLink(ctx, magnetId):
+        url = str(
+                "https://api.alldebrid.com/v4/magnet/status?agent="
+                + debrid_host
+                + "&apikey="
+                + debrid_key
+                + "&id="
+                + magnetId
+        )
+        print("got unhosted link")
+        t = json.loads(requests.get(url).text)["data"]["magnets"]["links"][0]["link"]
+        print(t)
+        return t
+
+    def magnetStatus(ctx, magnetId):
+        url = str(
+                "https://api.alldebrid.com/v4/magnet/status?agent="
+                + debrid_host
+                + "&apikey="
+                + debrid_key
+                + "&id="
+                + magnetId
+        )
+        ready = json.loads(requests.get(url).text)["data"]["magnets"]["status"]
+        print(ready)
+        if ready == "Ready":
+            return True
+        else:
+            return False
+
+    @commands.command(name="search", help="search 1337x")
+    async def search(self, ctx, arg):
+        print("search: " + ctx.message.content[8:])
+        magnetLink = torrents.info(
+            torrentId=torrents.search(
+                arg, sortBy="seeders", order="desc"
+                )['items'][0]['torrentId']
+                )['magnetLink']
         
-        locked = str(t[0]["link"])
-        url = str('https://api.alldebrid.com/v4/link/unlock?agent=' + debrid_host + '&apikey=' + debrid_key + '&link=' + locked)
-        r = requests.get(url)       
-        t = json.loads(r.text)["data"]
-    
-        embed = discord.Embed(title=t["filename"], description=t["link"])
-        
-        await ctx.send(embed=embed)
+        print("Magnet sent")
+        id = self.getMagnetId(self.addMagnet(magnetLink))
+        print(id)
+        if self.magnetStatus(id):
+            link = self.unlockLink(self.getUnhostedLink(id))
+        else:
+            print("uhhhhhh")
+
+        #embed = discord.Embed(title=t["filename"], description=t["link"])
+        print("Link sent")
+        #await ctx.send(embed=embed)
         # await ctx.send(str(t["filename"]))
-        # await ctx.send(str(t["link"]))
-    else:
-        await ctx.send("hold up")
+        await ctx.send(link)
 
-def setup(bot:commands.Bot):
-	bot.add_cog(DebridCog(bot))
+#    @commands.command(name="magnet", help="add magnet link")
+#    async def addMagnet(self, ctx, arg)
+
+def setup(bot: commands.Bot):
+    bot.add_cog(DebridCog(bot))
--- test.py
+++ test.py
@@ -3,5 +3,8 @@ from py1337x import py1337x
 
 torrents = py1337x(proxy='1337x.to', cache='cache', cacheTime=5000)
 torrentResults = torrents.search('no way home', sortBy='seeders', order='desc')
-
-print(torrents.info(torrentId=torrentResults['items'][0]['torrentId'])['magnetLink'])
+#print(torrentResults)
+#print(torrents.info(torrentId=torrentResults['items'][0]['torrentId'])['magnetLink'])
+#torrentId = 
+magnetLink = torrents.info(torrentId=torrentResults['items'][0]['torrentId'])['magnetLink']
+print(magnetLink)
